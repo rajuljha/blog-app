92158da3dde2c03d8d055297799180670bf128e2
Rajul Jha <rajuljha49@gmail.com>
2023-05-07 16:05:45 +0530

    Feature: Add update and delete posts.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 # Create your views here.
 
@@ -15,3 +16,13 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = ['title', 'author', 'body']
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
